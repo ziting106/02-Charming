e0b75833473fdc3c72f1a97616356bd1d1726f6a
ziting106 <99373797+ziting106@users.noreply.github.com>
2022-05-18 15:05:17 +0800

    全域margin

--- src/Home/pages/loginHome/LoginHome.js
+++ src/Home/pages/loginHome/LoginHome.js
@@ -1,31 +1,31 @@
 import React from "react";
 import Style from "./LoginHome.module.css";
 import LoginNav from "../../components/loginNav/LoginNav";
-import { AiTwotoneSound } from 'react-icons/ai'
+import { AiTwotoneSound } from "react-icons/ai";
 import Card from "../../components/card/Card";
 import users from "../../mockdata/users.json";
 
-function LoginHome(){
+function LoginHome() {
   return (
     <header>
       <LoginNav />
-      <div className={Style.pageMargin}>
-        <div className={Style.carousel}>
-          <AiTwotoneSound/>
-          <p className={Style.middleText}>
-            指揮中心快訊：新增75例COVID-19確定病例，分別為2例本土及73例境外移入
-          </p>
-        </div>
-        <div>
-          <ul className={Style.cardFlex}>
-            {users.map((r)=>(
-              <Card id={r.id} email={r.email} name={r.name}/>
-            ))}
-          </ul>
-        </div>
-      </div>
+      <hgroup>
+      <p className={Style.carousel}>
+        <AiTwotoneSound />
+        <p className={Style.middleText}>
+          指揮中心快訊：新增75例COVID-19確定病例，分別為2例本土及73例境外移入
+        </p>
+      </p>
+      </hgroup>
+      <section>
+        <ul className={Style.cardFlex}>
+          {users.map((r) => (
+            <Card id={r.id} email={r.email} name={r.name} />
+          ))}
+        </ul>
+      </section>
     </header>
   );
-};
+}
 
 export default LoginHome;
