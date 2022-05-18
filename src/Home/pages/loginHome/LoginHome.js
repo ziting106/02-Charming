8755cf9e358636e75e8c2f2d5f76f23f34225adf
ziting106 <99373797+ziting106@users.noreply.github.com>
2022-05-18 14:40:52 +0800

    卡片可滾動

--- src/Home/pages/loginHome/LoginHome.js
+++ src/Home/pages/loginHome/LoginHome.js
@@ -1,5 +1,5 @@
 import React from "react";
-import style from "./LoginHome.module.css";
+import Style from "./LoginHome.module.css";
 import LoginNav from "../../components/loginNav/LoginNav";
 import { AiTwotoneSound } from 'react-icons/ai'
 import Card from "../../components/card/Card";
@@ -9,15 +9,15 @@ function LoginHome(){
   return (
     <header>
       <LoginNav />
-      <div className={style.pageMargin}>
-        <div className={style.carousel}>
+      <div className={Style.pageMargin}>
+        <div className={Style.carousel}>
           <AiTwotoneSound/>
-          <p className={style.middleText}>
+          <p className={Style.middleText}>
             指揮中心快訊：新增75例COVID-19確定病例，分別為2例本土及73例境外移入
           </p>
         </div>
         <div>
-          <ul>
+          <ul className={Style.cardFlex}>
             {users.map((r)=>(
               <Card id={r.id} email={r.email} name={r.name}/>
             ))}
