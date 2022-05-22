2341a9a5cb5614f496479b16a3f46fc9fcf15d97
ziting106 <99373797+ziting106@users.noreply.github.com>
2022-05-22 19:09:49 +0800

    phoneFooter finish

--- src/Home/components/PhoneFooter/PhoneFooter.js
+++ src/Home/components/PhoneFooter/PhoneFooter.js
@@ -1,24 +1,35 @@
 import React from "react";
 import Style from "./PhoneFooter.module.css";
-import logo from "../../Assets/charming_logo.png";
+import { ImProfile } from "react-icons/im";
+import { MdOutlineVolunteerActivism } from "react-icons/md";
+import { FaUser } from "react-icons/fa";
+import { GiOpenBook, GiDiscussion } from "react-icons/gi";
 function PhoneFooter() {
   return (
-    <nav>
+    <div>
       <div className={Style.phoneFooter}>
-        <a href="loginHome">
-          <img src={logo} alt="logo" />
-        </a>
         <a href="Portfolio">
+          <ImProfile className={Style.icon}/>
           <p>柴米人</p>
         </a>
         <a href="Blog">
-          <li>柴訊</li>
+          <GiOpenBook className={Style.icon}/>
+          <p>柴訊</p>
+        </a>
+        <a href="/Product">
+          <MdOutlineVolunteerActivism className={Style.icon}/>
+          <p>精選商品</p>
         </a>
         <a href="Communication">
-          <li>柴社</li>
+          <GiDiscussion className={Style.icon}/>
+          <p>柴社</p>
+        </a>
+        <a href="/account">
+          <FaUser className={Style.icon}/>
+          <p>會員中心</p>
         </a>
       </div>
-    </nav>
+    </div>
   );
 }
 
