0fd979306dc5ea5a2a7c3f277cbd72d4ae766a25
ziting106 <99373797+ziting106@users.noreply.github.com>
2022-05-25 22:38:47 +0800

    產品詳細頁面

--- fontend/src/Home/Pages/ProductPage/ProductPage.js
+++ fontend/src/Home/Pages/ProductPage/ProductPage.js
@@ -6,14 +6,50 @@
 import React from "react";
 import LoginNav from "../../Components/LoginNav/LoginNav";
 import style from "./ProductPage.module.css";
+import { FcLikePlaceholder, FcLike } from "react-icons/fc";
+import Designer from "../../Components/Designer/Designer";
 
 function Product() {
   return (
     <>
       <LoginNav />
+      {/* 商品名稱 */}
       <section className={style.ProductPage}>
-      <h3>商品總攬 Page</h3>
-      
+        <h3>商品總攬 Page</h3>
+        {/* 圖片放置區 */}
+        <div className={style.displayFlex}>
+          <div className={style.bigImg}>
+            <a href=""></a>
+          </div>
+          <div className={style.smallImg}>
+            <a href="" ></a>
+            <a href="" ></a>
+            <a href="" ></a>
+            <a href="" ></a>
+          </div>
+        </div>
+        <div className={style.displayFlex}>
+          <p >刊登時間{}</p>
+          <p>檔案格式{}</p>
+        </div>
+        {/* 價格，數量，加入購物車按鈕，收藏按鈕 */}
+        <div>
+          <p>price</p>
+          <p>購買數量</p>
+          <div>
+            <button>加入購物車</button>
+            <button>收藏商品</button>
+          </div>
+        </div>
+        {/* 商品簡介 */}
+        <article className={style.displayFlex}>
+          <div>商品介紹</div>
+          <div>
+        {/* 關於設計師 */}
+            <div>關於設計師</div>
+            <Designer />
+          </div>
+        </article>
       </section>
     </>
   );
