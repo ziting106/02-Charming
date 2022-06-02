efd7209eb649f72e62e7c4441b30b27145a6bae2
ziting106 <99373797+ziting106@users.noreply.github.com>
2022-06-02 16:19:58 +0800

    processBar ok

--- fontend/src/Sales/Components/Processbar/Processbar.js
+++ fontend/src/Sales/Components/Processbar/Processbar.js
@@ -1,46 +1,53 @@
-import React from 'react'
-import './ProcessBar.css'
+import React from "react";
+import style from "./ProcessBar.module.css";
 function Processbar(props) {
-  const { step } = props
-  let start = step
+  const { step } = props;
+  let start = step;
   return (
-    <>
-      <div className="progressBar">
-        <div>
-          <div className="progressRound progressActive"></div>
-          <div>步驟一</div>
-          <div>購物車</div>
+    <section>
+      <div className={style.progressBar}>
+        <div className={style.center}>
+          <div
+            className={`${style.progressRound} ${style.progressActive}`}
+          ></div>
+          <p>步驟一：購物車</p>
         </div>
         {start >= 2 ? (
-          <div className="progressLine progressActive"></div>
+          <div
+            className={`${style.progressLine} ${style.progressActive}`}
+          ></div>
         ) : (
-          <div className="progressLine"></div>
+          <div className={style.progressLine}></div>
         )}
-        <div>
+        <div className={style.center}>
           {start >= 2 ? (
-            <div className="progressRound progressActive"></div>
+            <div
+              className={`${style.progressRound} ${style.progressActive}`}
+            ></div>
           ) : (
-            <div className="progressRound"></div>
+            <div className={style.progressRound}></div>
           )}
-          <div>步驟二</div>
-          <div>填資料</div>
+          <p>步驟二：填資料</p>
         </div>
         {start > 2 ? (
-          <div className="progressLine progressActive"></div>
+          <div
+            className={`${style.progressLine} ${style.progressActive}`}
+          ></div>
         ) : (
-          <div className="progressLine"></div>
+          <div className={style.progressLine}></div>
         )}
-        <div>
+        <div className={style.center}>
           {start > 2 ? (
-            <div className="progressRound progressActive"></div>
+            <div
+              className={`${style.progressRound} ${style.progressActive}`}
+            ></div>
           ) : (
-            <div className="progressRound"></div>
+            <div className={style.progressRound}></div>
           )}
-          <div>步驟三</div>
-          <div>付款</div>
+          <p>步驟三：付款</p>
         </div>
       </div>
-    </>
-  )
+    </section>
+  );
 }
-export default Processbar
+export default Processbar;
