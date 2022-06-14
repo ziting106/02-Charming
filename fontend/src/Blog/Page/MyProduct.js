f113510b7a6431ed6ceceb348fbf493e8d5cd408
ziting106 <99373797+ziting106@users.noreply.github.com>
2022-06-14 15:32:24 +0800

    整合賣家中心、會員中心、商品頁

---
+++ fontend/src/Blog/Page/MyProduct.js
@@ -0,0 +1,19 @@
+// component
+import Banner from '../Components/MyProductHeader/Banner'
+import PersonalInfo from '../Components/MyProductHeader/PersonalInfo'
+import LoginNav from '../../Home/Components/LoginNav/LoginNav'
+import MyProduct from '../../Home/Pages/MyProduct/MyProduct'
+
+const MyProductHeader = () => {
+  let now = localStorage.getItem('auth')
+  return (
+    <>
+      <LoginNav />
+      <Banner />
+      <PersonalInfo />
+      <MyProduct />
+    </>
+  )
+}
+
+export default MyProductHeader
